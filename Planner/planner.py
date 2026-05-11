f6cfed2b8e125e336afe11b4b16ec8b687245b9f
Christopher Eberhardt <christophereberhardt@Mac-mini.attlocal.net>
2026-05-11 15:07:01 -0500

    Basic planner

--- Planner/planner.py
+++ Planner/planner.py
@@ -70,6 +70,60 @@ from reportlab.lib.units import inch
 import reportlab.lib.enums 
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Frame, Spacer, Paragraph, PageBreak, Image
+from dataclasses import dataclass
+
+@dataclass
+class Point:
+    x: float
+    y: float
+
+class Page:
+    mid = Point(0,0) # mid.x, mid.y for center of page
+    max = Point(0,0) # max.x, max.y for top right corner of page
+    
+    def __init__(self, pageType='blank', colorFrame=None, title=None):
+        self.pageType = pageType
+        self.colorFrame = colorFrame
+        self.title = title
+        self.gridSpacing = 0.25*inch
+
+    def draw(self, canvas, rotate, corner):
+        canvas.saveState()
+        if rotate: 
+            width, height = canvas._pagesize
+            canvas.translate(width/2, height/2)
+            canvas.rotate(180)
+            canvas.translate(-width/2, -height/2)
+        canvas.translate(corner.x, corner.y)
+        if self.colorFrame:
+            #Frame(0,0, Page.max.x, Page.max.y).drawBoundary(canvas)
+            canvas.setStrokeColor(self.colorFrame)
+            canvas.rect(0,0, Page.max.x, Page.max.y)
+        
+        match self.pageType:
+            case 'blank':
+                pass
+            case 'grid':
+                canvas.setStrokeColor(colors.lightgrey)
+                xlist = []
+                x=0
+                while x < Page.max.x:
+                    xlist.append(x)
+                    x += self.gridSpacing
+                ylist = []
+                y=0
+                while y < Page.max.y:
+                    ylist.append(y)
+                    y += self.gridSpacing
+                canvas.grid(xlist,ylist)
+            case _:
+                canvas.setFont("Helvetica", 12)
+                canvas.setFillColor(colors.blue)
+                if self.title != None:
+                    canvas.drawCentredString(Page.mid.x, Page.mid.y, self.title)
+
+        canvas.restoreState()
+
 
 class Planner: 
     def __init__(self,nameOut="output.pdf", docSize=letter, marginSize=0.2*inch, showFrames=True, drawFolds=True, separator=False):
@@ -79,7 +133,6 @@ class Planner:
         self.drawFolds = drawFolds # draw the fold lines or not
         self.frameN = 0 # control variable
         self.canvas = None
-        self.layout = None
         self.nameOut = nameOut
         self.fontSize = None
         self.separator = separator # put a spacer after every paragraph
@@ -88,19 +141,48 @@ class Planner:
         self.subject = None # Metadata
         self.keywords = None # Metadata
         self.version = None
+        self.page1 = Page(pageType='blank', colorFrame=colors.black, title="Page 1")
+        self.page2 = Page(pageType='blank', colorFrame=colors.red, title="Page 2")
+        self.page3 = Page(pageType='blank', colorFrame=colors.green, title="Page 3")
+        self.page4 = Page(pageType='blank', colorFrame=colors.orange, title="Page 4")
+        self.page5 = Page(pageType='grid', colorFrame=colors.purple, title="Page 5")
+        self.page6 = Page(pageType='blank', colorFrame=colors.brown, title="Page 6")
+ 
     
     def create(self):
-        if self.layout == None: self.layout = 8
-        if self.layout == 8 or self.layout == 2: self.docSize = landscape(self.docSize) # other functions dependent upon layout
+        self.docSize = landscape(self.docSize) 
         if self.fontSize == None:
             self.fontSize = 8
         self.frames, self.frameRotate = self.defineFrames()
+        self.corners = self.computePaneCorners()
+        #Pane computation max, mid
+        Page.mid.x = self.frames[0]._width / 2
+        Page.mid.y = self.frames[0]._height / 2
+        Page.max.x = self.frames[0]._width
+        Page.max.y = self.frames[0]._height
+
         self.currentStyle = self.buildParagraphStyle(fontSize=self.fontSize, spaceAfter= 0)
         self.canvas = Canvas(self.nameOut, pagesize=self.docSize)
         self.frameN = 0
-        self.currentFrame = self.frames[self.frameN]
-        if self.showFrames: self.currentFrame.drawBoundary(self.canvas)
         if self.drawFolds: self.drawFoldlines() #self.canvas)
+        self.page1.draw(self.canvas, False,self.corners[1])
+        self.page2.draw(self.canvas, False,self.corners[2])
+        self.page5.draw(self.canvas, True,self.corners[0])
+
+    def computePaneCorners(self):
+        width, height = self.docSize
+        margin = self.margin
+        # 6 5 4 3 upside down
+        # 7 0 1 2
+        fWidth = (width / 4) - margin*2
+        fHeight = (height / 2) - margin*2
+
+        f0 = Point(0*fWidth+1*margin, 0*fHeight+1*margin)
+        f1 = Point(1*fWidth+3*margin, 0*fHeight+1*margin)
+        f2 = Point(2*fWidth+5*margin, 0*fHeight+1*margin)
+        f3 = Point(3*fWidth+7*margin, 0*fHeight+1*margin)
+        corners = [f0,f1,f2,f3]
+        return corners
 
     def defineFrames(self):
         width, height = self.docSize
@@ -121,7 +203,9 @@ class Planner:
         f6 = defineFrame(2*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
         f7 = defineFrame(3*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
 
+
         frames = [f1, f2, f3, f4, f5, f6, f7, f0]
+        frames = [f0,f1,f2,f3]
         rotate = [False, False, False, True, False, False, False, True]
 
         return frames, rotate
@@ -171,6 +255,7 @@ class Planner:
         self.canvas.translate(-self.docSize[0]/2, -self.docSize[1]/2)
 
     def makePlanner(self, inConfig):
+        self.create()
         self.build()
         
     def build(self):
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ pocket-pdf | Fifth version of pocket pdf project
 p5Alpha | Simple p5js programs/sketches
 Pico8 | Simple Pico8 programs
 pArcade | Learning Python Arcade library
+Planner | Pocket 8-page planner
