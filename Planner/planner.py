c8f82345e6e116e9603c041109cbefdc45a60d5a
Christopher Eberhardt <christophereberhardt@Mac-mini.attlocal.net>
2026-05-12 12:15:22 -0500

    Page factory implemented with argument passing including default font size

--- Planner/planner.py
+++ Planner/planner.py
@@ -71,23 +71,65 @@ import reportlab.lib.enums
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import Frame, Spacer, Paragraph, PageBreak, Image
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
+import shlex
 
 @dataclass
 class Point:
     x: float
     y: float
 
-class Page:
+def to_reportlab_color(val):
+    if not isinstance(val, str):
+        return val
+    
+    clean_val = val.replace('colors.','').strip().lower()
+    if clean_val.startswith('#') or clean_val.startswith('#'):
+        return colors.HexColor(clean_val)
+    try: # handle named colors
+        print (f"Trying to convert color value '{val}' to a ReportLab color")
+        return getattr(colors, clean_val, colors.yellowgreen)
+    except AttributeError:
+        print (f"ValueError(Unknown color: {val} using black")
+        return colors.black
+    
+class Page(ABC):
     mid = Point(0,0) # mid.x, mid.y for center of page
     max = Point(0,0) # max.x, max.y for top right corner of page
-    
-    def __init__(self, pageType='blank', colorFrame=None, title=None):
-        self.pageType = pageType
-        self.colorFrame = colorFrame
-        self.title = title
-        self.gridSpacing = 0.25*inch
+    fontsize = 10
 
-    def draw(self, canvas, rotate, corner):
+    def __init__(self, **kwargs):
+        kwargs = self._convert_types(kwargs)
+
+        # Handle defaults
+        if 'fontsize' in kwargs:
+            self.fontsize = int(kwargs.pop('fontsize'))
+                                      
+        for key,value in kwargs.items():
+            setattr(self, key, value)
+
+    CONVERTERS = {
+        'colorFrame': to_reportlab_color,
+        'date': lambda v: datetime.datetime.strptime(v, '%Y-%m-%d').date(),
+        'daystart': lambda v: datetime.datetime.strptime(v, '%H:%M').time(),
+    }
+
+    def _convert_types(self, params):
+        for key, values in params.items():
+            if key in self.CONVERTERS:
+                try:
+                    params[key] = self.CONVERTERS[key](values)
+                except Exception as e:
+                    print(f"Error converting parameter '{key}' with value '{values}': {e}")
+                    raise
+        return params
+    
+    @abstractmethod
+    def draw(self, canvas):
+        pass
+
+    def render(self, canvas, rotate, corner):
+        print (f"Rendering page with title {self.title} at corner {corner} with rotate={rotate}")
         canvas.saveState()
         if rotate: 
             width, height = canvas._pagesize
@@ -95,34 +137,71 @@ class Page:
             canvas.rotate(180)
             canvas.translate(-width/2, -height/2)
         canvas.translate(corner.x, corner.y)
-        if self.colorFrame:
-            #Frame(0,0, Page.max.x, Page.max.y).drawBoundary(canvas)
+        colorFrame = getattr(self, 'colorFrame', None)
+        if colorFrame is not None:
+            print (f"Drawing frame for page with title {self.title} using color {colorFrame}")
             canvas.setStrokeColor(self.colorFrame)
             canvas.rect(0,0, Page.max.x, Page.max.y)
-        
-        match self.pageType:
-            case 'blank':
-                pass
-            case 'grid':
-                canvas.setStrokeColor(colors.lightgrey)
-                xlist = []
-                x=0
-                while x < Page.max.x:
-                    xlist.append(x)
-                    x += self.gridSpacing
-                ylist = []
-                y=0
-                while y < Page.max.y:
-                    ylist.append(y)
-                    y += self.gridSpacing
-                canvas.grid(xlist,ylist)
-            case _:
-                canvas.setFont("Helvetica", 12)
-                canvas.setFillColor(colors.blue)
-                if self.title != None:
-                    canvas.drawCentredString(Page.mid.x, Page.mid.y, self.title)
-
+        self.draw(canvas)
         canvas.restoreState()
+
+
+    
+class BlankPage(Page):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def draw(self, canvas):
+        print(f"Blank page with title {self.title}")
+        pass
+
+class GridPage(Page):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def draw(self, canvas):
+        pass
+
+class WordPage(Page):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def draw(self, canvas):
+        canvas.setFont("Helvetica", self.fontsize)
+        title = getattr(self, 'title', 'Word Page')
+        canvas.drawCentredString(Page.mid.x, Page.mid.y, title)
+
+class PageFactory:
+    _registry = {
+        'blank': BlankPage,
+        'grid': GridPage,
+        'word':  WordPage,
+    }
+
+    @classmethod
+    def create_page(cls, page_type, **kwargs):
+        page_class = cls._registry.get(page_type.lower())
+        if page_class is None:
+            raise ValueError(f"Unknown page type: {page_type}")
+        return page_class(**kwargs)
+
+class PlannerParser:
+    @staticmethod
+    def parse_line(line, factory):
+        tokens = shlex.split(line)
+        if not tokens:
+            return None
+        
+        page_type = tokens[0].lower()
+        kv_pairs = tokens[1:]
+        kwargs = {}
+        for kv in kv_pairs:
+            if '=' in kv:
+                key, value = kv.split('=', 1)
+                kwargs[key] = value.rstrip(',')
+            else:
+                print(f"Warning: Ignoring invalid token '{kv}' in line: {line}")
+        return factory.create_page(page_type, **kwargs) 
 
 
 class Planner: 
@@ -131,7 +210,6 @@ class Planner:
         self.margin = marginSize # how big is the margin for each frame
         self.showFrames = showFrames # show the frame borders
         self.drawFolds = drawFolds # draw the fold lines or not
-        self.frameN = 0 # control variable
         self.canvas = None
         self.nameOut = nameOut
         self.fontSize = None
@@ -141,33 +219,33 @@ class Planner:
         self.subject = None # Metadata
         self.keywords = None # Metadata
         self.version = None
-        self.page1 = Page(pageType='blank', colorFrame=colors.black, title="Page 1")
-        self.page2 = Page(pageType='blank', colorFrame=colors.red, title="Page 2")
-        self.page3 = Page(pageType='blank', colorFrame=colors.green, title="Page 3")
-        self.page4 = Page(pageType='blank', colorFrame=colors.orange, title="Page 4")
-        self.page5 = Page(pageType='grid', colorFrame=colors.purple, title="Page 5")
-        self.page6 = Page(pageType='blank', colorFrame=colors.brown, title="Page 6")
+        self.page1 = PageFactory.create_page('blank', colorFrame=colors.black, title="Page 1")
+        self.page2 = PlannerParser.parse_line('word colorFrame=colors.blue, fontsize=24 title="Second Page"', PageFactory)
+        self.page3 = PlannerParser.parse_line('word colorFrame=colors.orange, title="Page 3"', PageFactory)
  
     
     def create(self):
         self.docSize = landscape(self.docSize) 
         if self.fontSize == None:
             self.fontSize = 8
-        self.frames, self.frameRotate = self.defineFrames()
         self.corners = self.computePaneCorners()
-        #Pane computation max, mid
-        Page.mid.x = self.frames[0]._width / 2
-        Page.mid.y = self.frames[0]._height / 2
-        Page.max.x = self.frames[0]._width
-        Page.max.y = self.frames[0]._height
+        Page.max.x = (self.docSize[0] - 8*self.margin) / 4
+        Page.max.y = (self.docSize[1] - 4*self.margin) / 2
+        Page.mid.x = Page.max.x / 2
+        Page.mid.y = Page.max.y / 2
 
-        self.currentStyle = self.buildParagraphStyle(fontSize=self.fontSize, spaceAfter= 0)
+        #Pane computation max, mid
+        Page.max.x = (self.docSize[0] - 8*self.margin) / 4
+        Page.max.y = (self.docSize[1] - 4*self.margin) / 2
+        Page.mid.x = Page.max.x / 2
+        Page.mid.y = Page.max.y / 2
+
         self.canvas = Canvas(self.nameOut, pagesize=self.docSize)
         self.frameN = 0
         if self.drawFolds: self.drawFoldlines() #self.canvas)
-        self.page1.draw(self.canvas, False,self.corners[1])
-        self.page2.draw(self.canvas, False,self.corners[2])
-        self.page5.draw(self.canvas, True,self.corners[0])
+        self.page1.render(self.canvas, False, self.corners[1])
+        self.page2.render(self.canvas, False, self.corners[2])
+        self.page3.render(self.canvas, True, self.corners[0])
 
     def computePaneCorners(self):
         width, height = self.docSize
@@ -184,62 +262,6 @@ class Planner:
         corners = [f0,f1,f2,f3]
         return corners
 
-    def defineFrames(self):
-        width, height = self.docSize
-        def defineFrame(x,y, w,h, m):
-            return Frame(x+m, y+m, w-m-m, h-m-m)
-        # 6 5 4 3 upside down
-        # 7 0 1 2
-        fWidth = width / 4
-        fHeight = height / 2
-
-        f0 = defineFrame(0*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        f1 = defineFrame(1*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        f2 = defineFrame(2*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        f3 = defineFrame(3*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        # top half (upside down)
-        f4 = defineFrame(0*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        f5 = defineFrame(1*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        f6 = defineFrame(2*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-        f7 = defineFrame(3*fWidth, 0*fHeight, fWidth, fHeight, self.margin)
-
-
-        frames = [f1, f2, f3, f4, f5, f6, f7, f0]
-        frames = [f0,f1,f2,f3]
-        rotate = [False, False, False, True, False, False, False, True]
-
-        return frames, rotate
-
-    def buildParagraphStyle(self, name='CurrentStyle',
-            textColor=colors.black,
-            backColor=colors.white,
-            alignment=reportlab.lib.enums.TA_LEFT,
-            align=None,
-            firstLineIndent=0,
-            leftIndent=0,
-            bulletIndent=0,
-            fontName='Times', #'Helvetica',
-            fontSize=10,
-            spaceBefore=0,
-            spaceAfter=None,
-            leading=None):
-        if leading == None: leading = int(fontSize * 1.2)
-        if spaceAfter == None: spaceAfter = int(fontSize * 1.2)
-        tempAlign = alignment if align == None else self.alignmentStrToEnum(align.lower())
-        return ParagraphStyle(
-            name=name,
-            backColor = backColor,
-            textColor = textColor,
-            alignment = tempAlign,
-            firstLineIndent = firstLineIndent,
-            leftIndent=leftIndent,
-            bulletIndent=bulletIndent,
-            fontName=fontName,
-            fontSize=fontSize,
-            spaceBefore=spaceBefore,
-            spaceAfter=spaceAfter,
-            leading=leading)
-
     def drawFoldlines(self):
         self.canvas.saveState()
         self.canvas.setDash(1,5) # on off
@@ -248,11 +270,6 @@ class Planner:
         for x in [2.75, 5.5, 8.25]: #TODO adjust to doc size
             self.canvas.line(x*inch, 0*inch, x*inch, 8.5*inch)
         self.canvas.restoreState()
-
-    def RotatePage(self):
-        self.canvas.translate(self.docSize[0]/2, self.docSize[1]/2)
-        self.canvas.rotate(180)
-        self.canvas.translate(-self.docSize[0]/2, -self.docSize[1]/2)
 
     def makePlanner(self, inConfig):
         self.create()
